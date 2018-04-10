f2463fed909366eb8e4aee0e82f2619a623227f1
HawkDon <Oliver.Loenning@hotmail.com>
2018-04-10 16:27:39 +0200

    Login 'virker' men kan forbedres

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -59,6 +59,7 @@ public class User implements Serializable {
     this.userName = userName;
     this.userPass = BCrypt.hashpw(userPass, BCrypt.gensalt(10));
   }
+  
 
   public String getUserName() {
     return userName;
